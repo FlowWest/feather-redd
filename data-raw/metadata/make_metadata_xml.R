010e31661009df6b45c7de3193563d1d94780a5e
alvizek <avizek@flowwest.com>
2024-11-15 10:11:27 -0800

    adds xml

--- data-raw/metadata/make_metadata_xml.R
+++ data-raw/metadata/make_metadata_xml.R
@@ -11,13 +11,13 @@ datatable_metadata <-
                 datatable_url = paste0("https://raw.githubusercontent.com/FlowWest/feather-redd/main/data/",
                                        c("redd_observations.csv")))
 
-# other_entity_metadata_1 <- list("file_name" = "unit_spatial_data.zip",
-#                                 "file_description" = "Kmz and GIS files for survey units",
-#                                 "file_type" = "zip",
-#                                 "physical" = create_physical("data-raw/metadata/unit_spatial_data.zip",
-#                                                              data_url = "https://raw.githubusercontent.com/FlowWest/feather-redd/main/data-raw/metadata/unit_spatial_data.zip"))
-# 
-# other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "zip"))
+other_entity_metadata_1 <- list("file_name" = "General_Chinook_Salmon_Redd_Survey_Methods_July_2024",
+                                "file_description" = "Survey Methods",
+                                "file_type" = "pdf",
+                                "physical" = create_physical("data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.pdf",
+                                                             data_url = "https://raw.githubusercontent.com/FlowWest/feather-redd/main/data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.pdf"))
+
+other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "pdf"))
 
 # save cleaned data to `data/`
 excel_path <- "data-raw/metadata/feather_redd_project_metadata.xlsx" 
@@ -46,11 +46,11 @@ dataset <- list() %>%
   add_other_entity(other_entity_metadata_1)
 
 # GO through and check on all units
-custom_units <- data.frame(id = c("redds", "salmon ", "decimal degrees", "decimal degrees"), #todo update custom units
-                           unitType = c("dimensionless", "dimensionless", "dimensionless", "dimensionless"),
-                           parentSI = c(NA, NA, NA, NA),
-                           multiplierToSI = c(NA, NA, NA, NA),
-                           description = c("parr marks", "decimal degrees", "decimal degrees","number of fish counted", "NTU", "square meters"))
+custom_units <- data.frame(id = c("redds", "salmon", "decimal degrees", "decimal degrees", "feet", "feet"), #todo update custom units
+                           unitType = c("dimensionless", "dimensionless", "dimensionless", "dimensionless", "dimensionless", "dimensionless"),
+                           parentSI = c(NA, NA, NA, NA, NA, NA),
+                           multiplierToSI = c(NA, NA, NA, NA, NA, NA),
+                           description = c("number of redds", "number of salmon ", "decimal degrees", "decimal degrees","feet", "feet"))
 
 
 unitList <- EML::set_unitList(custom_units)
