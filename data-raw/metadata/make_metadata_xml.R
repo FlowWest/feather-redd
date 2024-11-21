d051874df9cd281c3c0feea35e3f9e588729a202
Badhia <byuneskatz@flowwest.com>
2024-11-21 10:12:43 -0800

    runs xml, validates with no errors

--- data-raw/metadata/make_metadata_xml.R
+++ data-raw/metadata/make_metadata_xml.R
@@ -13,11 +13,11 @@ datatable_metadata <-
 
 other_entity_metadata_1 <- list("file_name" = "General_Chinook_Salmon_Redd_Survey_Methods_July_2024",
                                 "file_description" = "Survey Methods",
-                                "file_type" = "zip",
-                                "physical" = create_physical("data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.zip",
-                                                             data_url = "https://raw.githubusercontent.com/FlowWest/feather-redd/main/data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.zip"))
+                                "file_type" = "pdf",
+                                "physical" = create_physical("data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.pdf",
+                                                             data_url = "https://raw.githubusercontent.com/FlowWest/feather-redd/main/data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.pdf"))
 
-other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "zip"))
+other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "pdf"))
 
 # save cleaned data to `data/`
 excel_path <- "data-raw/metadata/feather_redd_project_metadata.xlsx" 
@@ -64,4 +64,4 @@ eml <- list(packageId = edi_number,
 edi_number
 EML::write_eml(eml, paste0(edi_number, ".xml"))
 EML::eml_validate(paste0(edi_number, ".xml"))
-EMLaide::evaluate_edi_package(Sys.getenv("edi_user_id"), Sys.getenv("edi_password"), paste0(edi_number, ".xml"))
+# EMLaide::evaluate_edi_package(Sys.getenv("edi_user_id"), Sys.getenv("edi_password"), paste0(edi_number, ".xml"))
