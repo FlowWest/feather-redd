24b7c72c8eb1eb88431edd652dd8c46f88463e60
Badhia <byuneskatz@flowwest.com>
2024-11-20 17:15:21 -0800

    updates script and project metadata

--- data-raw/metadata/make_metadata_xml.R
+++ data-raw/metadata/make_metadata_xml.R
@@ -13,11 +13,11 @@ datatable_metadata <-
 
 other_entity_metadata_1 <- list("file_name" = "General_Chinook_Salmon_Redd_Survey_Methods_July_2024",
                                 "file_description" = "Survey Methods",
-                                "file_type" = "pdf",
+                                "file_type" = "zip",
                                 "physical" = create_physical("data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.zip",
                                                              data_url = "https://raw.githubusercontent.com/FlowWest/feather-redd/main/data-raw/metadata/General_Chinook_Salmon_Redd_Survey_Methods_July_2024.zip"))
 
-other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "pdf"))
+other_entity_metadata_1$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "zip"))
 
 # save cleaned data to `data/`
 excel_path <- "data-raw/metadata/feather_redd_project_metadata.xlsx" 
