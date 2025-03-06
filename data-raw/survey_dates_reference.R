d034b88f998c93bdc011524b82cb8a7b996e047a
Badhia <byuneskatz@flowwest.com>
2025-03-05 17:55:02 -0800

    adds data entries with zeros to number_redds

--- data-raw/survey_dates_reference.R
+++ data-raw/survey_dates_reference.R
@@ -4,7 +4,9 @@ library(stringr)
 library(readxl)
 library(readr)
 
-# the goal of creating this table is to have a reference of when surveys were conducted
+# the goal of creating this table is to have a reference of when surveys were conducted.
+# also we are adding data entries for those dates when surveys were conducted but no data was collected due to redds not being observed
+
 # Feather redd team provided a table with date references for each year's survey week. We have debrief that into "survey_week_date_reference_2014_2023.csv"
 # for the survey weeks that do not correspond to a number, I am assigning a numeric value, for the sake of code simplicity
 
@@ -64,7 +66,9 @@ write_csv(surveyed_sites, "data/surveyed_sites_table.csv")
 # The goal below is to add redd data entries with a redd count of 0 for when surveys were preformed but no redds were observed
 # reading feather redd data (with survey week)
 
-redd_data <- read.csv("data-raw/qc-processing-files/survey_wk/redd_observations_survey_wk_clean.csv")
+redd_data <- read.csv("data-raw/qc-processing-files/survey_wk/redd_observations_survey_wk_clean.csv") |> 
+  mutate(date = as.Date(date)) |> 
+  glimpse()
 
 glimpse(survey_sites_clean)
 
@@ -75,10 +79,34 @@ surveyed_sites_summary <- survey_combined |>
   glimpse()
 
 yes_redd_data <- redd_data |> 
-  mutate(survey_week = as.numeric(survey_wk)) |> 
-  select(date, survey_week, location, latitude, longitude) |> 
+  mutate(survey_week = as.numeric(survey_wk),
+         year = year(date)) |> 
+  select(date, survey_week, location, year) |> 
   glimpse()
 
 no_redd_data <- surveyed_sites_summary |> 
-  left_join(yes_redd_data, by = c("survey_week", "location")) |> 
-  filter(is.na(date)) |> View()
+  left_join(yes_redd_data, by = c("survey_week", "location", "year")) |> 
+  filter(is.na(date)) |> # adding this to filter out when there are no redd data/dates associated with that survey_wk/location/year
+  glimpse()
+
+#checks to see if join shows what it isn't on data
+yes_redd_data |> 
+  filter(year == "2014", location == "belowbigholeeast") |> 
+  View()
+yes_redd_data |> 
+  filter(year == "2015", location == "developing") |> 
+   View()
+
+# adding those records on to redd data 
+zeros_added <- no_redd_data |> 
+  mutate(number_redds = 0,
+         date = start_date) |> # assigning start_date instead of the exact date
+  rename(survey_wk = survey_week) |> 
+  select(-c(start_date, end_date, surveyed, year)) |> 
+  glimpse()
+
+redd_data_updated <- redd_data |>
+  bind_rows(zeros_added)
+
+# save redd data with zeros added
+write.csv(redd_data_updated, "data/redd_observation_w_zeros.csv")
